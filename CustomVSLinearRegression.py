7138e379c0de17bf443b69fa7875549a6743ab02
rInasDS <73656371+rInasDS@users.noreply.github.com>
2022-05-22 17:10:33 +0300

    Update CustomVSLinearRegression.py

--- CustomVSLinearRegression.py
+++ CustomVSLinearRegression.py
@@ -13,6 +13,7 @@ class CustomLinearRegression:
     d = dict()
     
     def __init__(self, fit_intercept=True):
+
         self.fit_intercept = fit_intercept
         self.coefficient = ...
         self.intercept = ...
@@ -26,12 +27,16 @@ class CustomLinearRegression:
             self.X = X2
             self.B = np.linalg.solve(X2 @ X2.T, X2 @ y)
             self.B = np.insert(self.B, 0, 0)
-        else:
+            self.intercept = self.B[1]
+            self.coefficient = self.B[1:]
+            self.d = {'Intercept': self.intercept,
+                      'Coefficient': self.coefficient[1:]}
+        else:    # if self.fit_intercept is False:
             self.B = np.linalg.solve(X @ X.T, X @ y)
-        self.intercept = self.B[1]
-        self.coefficient = self.B[1:]
-        self.d = {'Intercept': self.intercept,  
-                  'Coefficient': np.array(self.B[2:])}  
+            self.intercept = self.B[0]
+            self.coefficient = self.B[0:]
+            self.d = {'Intercept': self.intercept,
+                      'Coefficient': self.coefficient}
 
     def predict(self, X):
         self.yhat = self.coefficient @ self.X
@@ -43,28 +48,28 @@ class CustomLinearRegression:
                                                            len(y) * sum(self.yhat) ** 2)) ** 0.5) ** 2
         self.d['R2'] = r2  
 
-    def rmse(self, y, yhat):    
+    def rmse(self, y, yhat):
         rmse = ((y - self.yhat) ** 2).mean() ** 0.5
         self.d['RMSE'] = rmse  
 
 
-x = np.array([2.31,   7.07 ,  7.07 ,  2.18  , 2.18 ,  2.18  , 7.87 ,  7.87 ,  7.87  , 7.87,7.87])
-w = np.array([65.2,   78.9 ,  61.1 ,  45.8  , 54.2 ,  58.7  , 66.6  , 96.1,  100. ,   85.9,94.3])
-s = np.array([15.3 ,  17.8 ,  17.8 ,  18.7,   18.7 ,  18.7,   15.2  , 15.2 ,  15.2 ,  15.2,15.2])
-y = np.array([24. , 21.6, 34.7, 33.4, 36.2, 28.7, 22.9, 27.1 ,16.5 ,18.9, 15.])
+x = np.array([2.31, 7.07, 7.07, 2.18, 2.18, 2.18, 7.87, 7.87, 7.87, 7.87, 7.87])
+w = np.array([65.2, 78.9, 61.1, 45.8, 54.2, 58.7, 66.6, 96.1, 100., 85.9,94.3])
+s = np.array([15.3, 17.8, 17.8, 18.7, 18.7, 18.7, 15.2, 15.2, 15.2, 15.2,15.2])
+y = np.array([24., 21.6, 34.7, 33.4, 36.2, 28.7, 22.9, 27.1, 16.5, 18.9, 15.])
 
-clr = CustomLinearRegression(fit_intercept=True)
+clr = CustomLinearRegression(fit_intercept=True)   
 clr.fit((np.vstack((x, w, s))), y)
 y_pred = clr.predict(np.vstack((x, w, s)))
 clr.r2_score(y, y_pred)
 clr.rmse(y, y_pred)
 
 
-# -----------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 #import pandas as pd
-import numpy as np
+#import numpy as np
 
 
 #df = pd.read_csv('data.csv')
@@ -72,14 +77,14 @@ import numpy as np
 #y = np.array(df['y'])
 X = np.array([x,w,s]).T
 y = np.array(y)
-model = LinearRegression().fit(X, y)
+model = LinearRegression(fit_intercept=True).fit(X, y)  
 y_pred = model.predict(X)
 inter = model.intercept_
 coef = model.coef_
 r2 = r2_score(y, y_pred)
 RMSE = np.sqrt(mean_squared_error(y, y_pred))
 
-
+# Comparing two dictionaries
 clr.d['Intercept'] -= inter
 clr.d['Coefficient'] -= coef
 clr.d['R2'] -= r2
